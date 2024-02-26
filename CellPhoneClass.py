07ff7d38a74cbcf7dd0c4cb7f3cdb95f60ded129
Grant <grant_mixon1@baylor.edu>
2024-02-26 14:10:33 -0600

    class, no finished

---
+++ CellPhoneClass.py
@@ -0,0 +1,26 @@
+
+
+class CellPhone:
+
+    def(__init__(self,ma,mo,rp)):
+        self.__manufact = ma 
+        self.__model = mo
+        self.__retail_price = rp
+    
+    def set_manufact(self,ma,mo,rp):
+        self.__manufact = ma
+    
+    def set_model(self,ma,mo,rp):
+        self.__model = mo
+
+    def set_retail_price(self,ma,mo,rp):
+        self.__retail_price = rp
+
+    def get_manufact(self,ma,mo,rp):
+        
+
+    def get_model(self,ma,mo,rp):
+
+
+    def get_retail_price(self,ma,mo,rp):
+    
--- InsectProgram.py
+++ InsectProgram.py
@@ -1,11 +1,11 @@
 import InsectClass as i
 
-mosquito = i.Insect()
-housefly = i.Insect()
+mosquito = i.Insect(2,4,'Mosquito')
+housefly = i.Insect(2,4,'Housefly')
 
 
 mosquito.calc_flight()
 housefly.calc_flight()
 
-print(f"the mosquito can fly up to {mosquito.get_miles()} miles")
-print(f"the housefly can fly up to {housefly.get_miles()} miles")
+print(f"the {mosquito.get_name()} can fly up to {mosquito.get_miles()} miles")
+print(f"the {housefly.get_name()} can fly up to {housefly.get_miles()} miles")
